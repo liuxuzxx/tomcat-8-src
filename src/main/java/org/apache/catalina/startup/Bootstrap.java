cac4e3bda72a7aea79849ff4c0435d1a80675f59
lxzxx <928525166@qq.com>
2017-08-22 22:57:30 +0800

    修改replace函数为String的replaceAll函数，作者估计是在jdk4的基础上开发的

--- src/main/java/org/apache/catalina/startup/Bootstrap.java
+++ src/main/java/org/apache/catalina/startup/Bootstrap.java
@@ -244,41 +244,9 @@ public final class Bootstrap {
 	 * @return the modified string
 	 */
 	protected String replace(String str) {
-		// Implementation is copied from ClassLoaderLogManager.replace(),
-		// but added special processing for catalina.home and catalina.base.
 		String result = str;
-		int pos_start = str.indexOf("${");
-		if (pos_start >= 0) {
-			StringBuilder builder = new StringBuilder();
-			int pos_end = -1;
-			while (pos_start >= 0) {
-				builder.append(str, pos_end + 1, pos_start);
-				pos_end = str.indexOf('}', pos_start + 2);
-				if (pos_end < 0) {
-					pos_end = pos_start - 1;
-					break;
-				}
-				String propName = str.substring(pos_start + 2, pos_end);
-				String replacement;
-				if (propName.length() == 0) {
-					replacement = null;
-				} else if (Globals.CATALINA_HOME_PROP.equals(propName)) {
-					replacement = getCatalinaHome();
-				} else if (Globals.CATALINA_BASE_PROP.equals(propName)) {
-					replacement = getCatalinaBase();
-				} else {
-					replacement = System.getProperty(propName);
-				}
-				if (replacement != null) {
-					builder.append(replacement);
-				} else {
-					builder.append(str, pos_start, pos_end + 1);
-				}
-				pos_start = str.indexOf("${", pos_end + 1);
-			}
-			builder.append(str, pos_end + 1, str.length());
-			result = builder.toString();
-		}
+		result = result.replaceAll(Globals.CATALINA_HOME_PROP,getCatalinaHome());
+		result = result.replaceAll(Globals.CATALINA_BASE_PROP,getCatalinaBase());
 		return result;
 	}
 
